5df695349fa05af2fbaa12f334f147288b71eabf
aakashhn <nakhodakash@gmail.com>
2023-10-19 17:41:21 +0530

    dao class commit

--- src/main/java/com/cg/dao/DepartmentDao.java
+++ src/main/java/com/cg/dao/DepartmentDao.java
@@ -1,11 +1,13 @@
 package com.cg.dao;
 
+import java.util.List;
+
+import com.cg.entity.Department;
+
 public interface DepartmentDao {
 
-		
+	String addDepartment(Department d);
 
-		
-
-	
+	List<Department> getAllDepartment();
 
 }
